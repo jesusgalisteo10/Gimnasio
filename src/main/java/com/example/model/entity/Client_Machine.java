0beb92539649b45bb8641abd8bd4b38259167da0
jesusgalisteo10 <jesusgalisteovalverde@gmail.com>
2026-05-06 12:46:18 +0200

    commit clase cliente y maquina - cliente

--- src/main/java/com/example/model/entity/Client_Machine.java
+++ src/main/java/com/example/model/entity/Client_Machine.java
@@ -1,1 +1,63 @@
+package com.example.model.entity;
+
+public class Client_Machine {
+    private String clientCode;
+    private String machineCode;
+
+    // Constructor vacío
+    public Client_Machine() {
+    }
+
+    // Constructor con parámetros
+    public Client_Machine(String clientCode, String machineCode) {
+        this.clientCode = clientCode;
+        this.machineCode = machineCode;
+    }
+
+    // Getters y Setters
+    public String getClientCode() {
+        return clientCode;
+    }
+
+    public void setClientCode(String clientCode) {
+        this.clientCode = clientCode;
+    }
+
+    public String getMachineCode() {
+        return machineCode;
+    }
+
+    public void setMachineCode(String machineCode) {
+        this.machineCode = machineCode;
+    }
+
+    // metodo toString()
+    @Override
+    public String toString() {
+        return "ClientMachine{" +
+                "clientCode='" + clientCode + '\'' +
+                ", machineCode='" + machineCode + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client_Machine that = (Client_Machine) o;
+
+        if (clientCode != null ? !clientCode.equals(that.clientCode) : that.clientCode != null) return false;
+        return machineCode != null ? machineCode.equals(that.machineCode) : that.machineCode == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = clientCode != null ? clientCode.hashCode() : 0;
+        result = 31 * result + (machineCode != null ? machineCode.hashCode() : 0);
+        return result;
+    }
+}
+
 
